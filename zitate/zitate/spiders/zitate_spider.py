36959b40837ca3febce1bccfd601d21f97926724
Simon Liedtke <liedtke.simon@googlemail.com>
2017-07-13 12:08:25 +0200

    [zitate] follow links to next pages

--- zitate/zitate/spiders/zitate_spider.py
+++ zitate/zitate/spiders/zitate_spider.py
@@ -5,7 +5,7 @@ class ZitateSpider(scrapy.Spider):
     name = 'zitate'
     start_urls = [
         'http://www.zitate.de/autor/Adams%2C+Scott',
-        'http://www.zitate.de/autor/Anonym'
+        'http://www.zitate.de/autor/Allen%2C+Woody',
     ]
 
     def parse(self, response):
@@ -15,3 +15,23 @@ class ZitateSpider(scrapy.Spider):
         self.log('author: {}'.format(author))
         for quote in quotes:
             yield {'author': author, 'quote': quote}
+
+        cur_page_number = get_page_number(response.url)
+
+        next_page = response.css('.next a::attr(href)').extract_first()
+        next_page_number = None
+        if next_page is not None:
+            next_page_number = get_page_number(next_page)
+
+        # there is always a "next" link, it references to current page
+        # if there are no following pages left
+        if next_page_number != cur_page_number:
+            yield response.follow(next_page, callback=self.parse)
+
+
+def get_page_number(url):
+    'Return the value for the parameter `page` in a URL like `?page=PAGE`'
+    page_numbers = urlparse.parse_qs(urlparse.urlparse(url).query).get('page')
+    if page_numbers is not None:
+        return page_numbers[0]
+    return ''
